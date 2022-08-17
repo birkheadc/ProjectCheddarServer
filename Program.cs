14a237fdf2402afbd9c038a98895bdc113c7b526
Colby Birkhead <birkheadc@gmail.com>
2022-08-18 05:31:58 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectCheddarServer;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting server...");
+        Console.Read();
+    }
+}
